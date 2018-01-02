16d5885d0bb3ef4c96611fb25ba09dcab5401edd
Ryan Fan <rfan.cn@gmail.com>
2018-01-02 15:19:55 +0800

    move dockerfile to root

--- misc/script/build.sh
+++ misc/script/build.sh
@@ -1,19 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -o errexit
-set -o nounset
-set -o pipefail
-
-wegigo_ROOT=$(dirname "${BASH_SOURCE}")/..
-
-
-KUBE_VERBOSE="${KUBE_VERBOSE:-1}"
-source "${KUBE_ROOT}/hack/lib/init.sh"
-
-kube::golang::build_binaries "$@"
-kube::golang::place_bins
-
-go get github.com/kabukky/httpscerts
-go get github.com/julienschmidt/httprouter
-go get github.com/rfancn/goy2h
-go build -o wegigo
+SRC_DIR=/go/src/github.com/rfancn/wegigo
+docker run --rm -v "$PWD":$SRC_DIR -w $SRC_DIR golang:1.8 make
