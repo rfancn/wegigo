f0bc8585da43b4fda99f30c65dc5799bb94f022f
Ryan Fan <rfan.cn@gmail.com>
2018-01-02 15:03:58 +0800

    add docker files

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run --rm -v "$PWD":/go/src/github.com/rfancn/wegigo -w /go/src/github.com/rfancn/wegigo golang:1.8 make
+SRC_DIR=/go/src/github.com/rfancn/wegigo
+docker run --rm -v "$PWD":$SRC_DIR -w $SRC_DIR golang:1.8 make
