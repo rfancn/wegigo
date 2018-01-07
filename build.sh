a3a48ffc655353a69166a3ca9d17affe21e2a2b8
Ryan Fan <rfan.cn@gmail.com>
2018-01-07 12:58:28 +0800

    fix build privilege issue

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 SRC_DIR=/go/src/github.com/rfancn/wegigo
-docker run --rm -v "$BASE_DIR":$SRC_DIR -w $SRC_DIR golang:1.8 make
+docker run --privileged=true --rm -v "$BASE_DIR":$SRC_DIR -w $SRC_DIR golang:1.8 make
